a6d6d623a0c07916da42a5879dff2167791082de
Marisssss <118815790+Marisssss@users.noreply.github.com>
2023-05-24 08:29:58 +0300

    Базовый класс модели представления

---
+++ Dot&Box/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Dot_Box.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangingEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));        
+        }
+
+        protected virtual bool Sel<T>(ref T field, T value, [CallerMemberName] string PeopserName= null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
